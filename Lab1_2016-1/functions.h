7f59f578d7c791fa9921ce8844fe30ff7ffadeaa
Fabricio Monsalve <fabricio2111@gmail.com>
2016-04-05 10:39:39 -0500

    impresion en desorden

---
+++ Lab1_2016-1/functions.h
@@ -0,0 +1,20 @@
+
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int pasarPalabra();
+void impLinea(char c, int num);
+int leerCiudad();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H */
+
