11e8ce85cd718bb0e4b49a7c8e3df1e5ceacd86c
Fabricio Monsalve <fabricio2111@gmail.com>
2016-04-29 00:05:48 -0500

    codigo comentado

--- Lab3_2013-1/functions.cpp
+++ Lab3_2013-1/functions.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 
 void agregarCaract(int *&buffCaract, int color, int estilo, int numPuntos){
-    buffCaract = new int[3];
+    buffCaract = new int[3]; //se le da tamaño exacto antes de asignarle los valores
     buffCaract[0] = color;
     buffCaract[1] = estilo;
     buffCaract[2] = numPuntos;
@@ -16,20 +16,20 @@ void agregarCaract(int *&buffCaract, int color, int estilo, int numPuntos){
 
 void leerCoordenadas(int **&buffCoord, int &numPuntos, int numPol){
     int x, y, indice=0;
-    int arrX[200], arrY[200];
+    int arrX[200], arrY[200]; //se crean arreglos de tamaño estatico grande para guardar las coordenadas provisionalmente
     while(cin >> x >> y){
         arrX[indice] = x;
         arrY[indice] = y;
         indice++;
     }
-    cin.clear();
-    int *buffX = new int[indice*2];
+    cin.clear(); //se limpian las flags
+    int *buffX = new int[indice*2]; //se le da memoria exacta de acuerdo al numero de coordenadas leidas
     int *buffY = new int[indice*2];
-    for (int i = 0; i < indice; i++) {
+    for (int i = 0; i < indice; i++) { //se transfieren los datos a los buffers de tamaño exacto
         buffX[i] = arrX[i];
         buffY[i] = arrY[i];
     }
-    buffCoord[numPol*2] = buffX;
+    buffCoord[numPol*2] = buffX; //por que se comienza siempre en un indice par, ya que va de 2 en 2 (0,2,4,6...)
     buffCoord[numPol*2+1] = buffY;
     numPuntos = indice*2;
 }
@@ -48,7 +48,7 @@ int leePoligonos(int **&lstCaract, int **&lstCoord){
         agregarCaract(buffCaract[numPol], color, estilo, numPuntos);  
         numPol++;
     }
-    memoriaExacta(lstCoord,buffCoord, numPol*2);
+    memoriaExacta(lstCoord,buffCoord, numPol*2); //es x2 porque se guarda X e Y de cada poligono en los indices 2n y 2n+1
     memoriaExacta(lstCaract,buffCaract, numPol);
     return numPol;
 }
